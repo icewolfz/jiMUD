c13be897cea3ce0d16be494684e3c34dfc072737
icewolfz <icewolfz@users.noreply.github.com>
2017-08-08 23:21:44 -0500

    Add menubar class to wrap updating system

---
+++ src/common/Menubar.ts
@@ -0,0 +1,99 @@
+import { remote } from 'electron';
+const { Menu, MenuItem, BrowserWindow } = remote;
+
+export enum ItemType {
+    menu = 0,
+    raw = 1,
+    both = 2
+}
+
+export class Menubar {
+    public window: Electron.BrowserWindow;
+    public menu: any[];
+    private _menubar;
+
+    constructor(menu: any[], window?: Electron.BrowserWindow) {
+        if (!window)
+            this.window = remote.getCurrentWindow();
+        else
+            this.window = window;
+        this.menu = menu;
+        this.rebuild();
+    }
+
+    public getItem(menu: (string | string[]), type?: ItemType) {
+        let item;
+        let i = 0;
+        let items;
+        let tItem;
+        let tItems;
+        if (!menu) return null;
+        if (!Array.isArray(menu))
+            menu = menu.split('|');
+        items = this._menubar.items;
+        tItems = menu;
+        for (i = 0; i < menu.length; i++) {
+            if (!items || items.length === 0) break;
+            for (let m = 0; m < items.length; m++) {
+                if (!items[m]) continue;
+                if (items[m].id === menu[i] || items[m].label.toLowerCase().replace(/&/g, '') === menu[i].toLowerCase()) {
+                    item = items[m];
+                    tItem = tItems[m];
+                    if (item.submenu) {
+                        items = item.submenu.items;
+                        tItems = tItem.submenu;
+                    }
+                    else
+                        items = null;
+                    break;
+                }
+            }
+        }
+        if (i !== menu.length)
+            return null;
+        if (type === ItemType.raw)
+            return tItem;
+        if (type === ItemType.both)
+            return [item, tItem];
+        return item;
+    }
+
+    public updateItem(menu: (string | string[]), options) {
+        let item;
+        let items;
+        let tItem;
+        if (menu) return;
+
+        items = this.getItem(menu, ItemType.both);
+        if (!items) return;
+        item = items[0];
+        tItem = items[1];
+
+        if (options.enabled != null)
+            item.enabled = options.enabled ? true : false;
+        if (options.checked != null)
+            item.checked = options.checked ? true : false;
+        if (options.icon != null)
+            item.icon = options.icon;
+        if (options.visible != null)
+            item.visible = options.visible ? true : false;
+        if (options.position != null)
+            item.position = options.position;
+
+        tItem.enabled = item.enabled;
+        tItem.checked = item.checked;
+        tItem.icon = item.icon;
+        tItem.visible = item.visible;
+        tItem.position = item.position;
+
+        if (options.submenu != null) {
+            tItem.submenu = options.submenu;
+            this.rebuild();
+        }
+    }
+
+    public rebuild() {
+        this._menubar = Menu.buildFromTemplate(this.menu);
+        this.window.setMenu(this._menubar);
+    }
+}
