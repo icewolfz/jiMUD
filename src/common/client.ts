9d332a898b0f420a140bfd1044bc0fc88e91daa1
icewolfz <icewolfz@users.noreply.github.com>
2017-12-05 21:34:50 -0600

    Spurce up error message display some

--- src/common/client.ts
+++ src/common/client.ts
@@ -1020,7 +1020,10 @@ export class Client extends EventEmitter {
         else
             msg = err;
 
-        this.echo('Error: ' + msg, AnsiColorCode.ErrorText, AnsiColorCode.ErrorBackground, true, true);
+        if (msg.match(/^.*Error: .*$/g))
+            this.echo(msg, AnsiColorCode.ErrorText, AnsiColorCode.ErrorBackground, true, true);
+        else
+            this.echo('Error: ' + msg, AnsiColorCode.ErrorText, AnsiColorCode.ErrorBackground, true, true);
 
         if (this.options.logging.errors) {
             fs.writeFileSync(parseTemplate(path.join('{data}', 'jimud.error.log')), new Date().toLocaleString() + '\n', { flag: 'a' });
