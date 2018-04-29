431d2f2916b686e25ba970a3cc2b4ccd72d08e3a
icewolfz <icewolfz@users.noreply.github.com>
2018-04-29 18:24:18 -0500

    Convert editbase to abstract and add several helper functions and update editors to use them. Code editor: add save system, change tracking and status bar

--- src/common/code.editor.ts
+++ src/common/code.editor.ts
@@ -1,6 +1,8 @@
 import EventEmitter = require('events');
+import { existsSync } from './library';
 const fs = require('fs-extra');
 const path = require('path');
+const chokidar = require('chokidar');
 
 declare let ace;
 
@@ -16,12 +18,22 @@ export enum UpdateType {
 export interface EditorOptions {
     file: string;
     container?: any;
+    open?: boolean;
+    new?: boolean;
 }
 
-export class EditorBase extends EventEmitter {
+export enum FileState {
+    closed = 0,
+    opened = 1,
+    changed = 2,
+    new = 4
+}
+
+abstract class EditorBase extends EventEmitter {
 
     private $parent: HTMLElement;
     private $file;
+    public state: FileState = FileState.closed;
 
     get file(): string {
         return this.$file;
@@ -46,6 +58,10 @@ export class EditorBase extends EventEmitter {
         else
             this.parent = document.body;
         this.file = options.file;
+        if (options.open)
+            this.open();
+        if (options.new)
+            this.state |= FileState.new;
     }
 
     set parent(parent) {
@@ -66,26 +82,40 @@ export class EditorBase extends EventEmitter {
 
     get parent(): HTMLElement { return this.$parent; }
 
-    public createControl() { }
+    abstract createControl();
 
-    public read() {
-        if (!this.$file || this.$file.length === 0)
+    public read(file?: string): string {
+        if (!file || file.length === 0)
+            file = this.file;
+        if (!file || file.length === 0)
             return '';
-        try {
-            return fs.readFileSync(this.$file);
-        }
-        catch (err) {
-            this.emit('error', err);
-        }
-        return '';
+        return fs.readFileSync(this.file);
     }
 
-    public write() {
-
+    public write(data, file?: string) {
+        if (!file || file.length === 0)
+            file = this.file;
+        if (!file || file.length === 0) {
+            throw new Error('Invalid file');
+        }
+        fs.writeFileSync(this.file, data);
     }
 
-    public refresh() {}
+    abstract refresh(): void;
 
+    abstract open(): void;
+
+    abstract save(): void;
+
+    abstract selected();
+
+    get changed(): boolean {
+        return (this.state & FileState.changed) === FileState.changed;
+    }
+
+    get new(): boolean {
+        return (this.state & FileState.new) === FileState.new;
+    }
 }
 
 export class CodeEditor extends EditorBase {
@@ -93,6 +123,7 @@ export class CodeEditor extends EditorBase {
     private $editorEl;
     private $editor;
     private $session;
+    private $statusbar: HTMLElement;
 
     public createControl() {
         if (this.$el) {
@@ -129,11 +160,40 @@ export class CodeEditor extends EditorBase {
             newLineMode: 'unix',
             tabSize: 3
         });
+        this.$statusbar = document.createElement('div');
+        this.$statusbar.id = this.parent.id + '-statusbar';
+        this.$statusbar.classList.add('statusbar');
+        this.$statusbar.innerHTML = '<span id="' + this.parent.id + '-filename"></span><span id="' + this.parent.id + '-filesize">File size: 0</span><span id="' + this.parent.id + '-statusmessage"></span>';
+        this.parent.appendChild(this.$statusbar);
+        let StatusBar = ace.require('ace/ext/statusbar').StatusBar;
+        new StatusBar(this.$editor, this.$statusbar);
 
+        this.$session.on('change', () => {
+            var d = this.$session.getValue();
+            this.state |= FileState.changed;
+            $('#' + this.parent.id + '-filesize').text("File size: " + d.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+            this.emit('changed');
+        });
+        this.emit('created');
     }
 
-    public openFile() {
-        if (!this.file || this.file.length === 0)
+    get file(): string {
+        return super.file;
+    }
+    set file(value: string) {
+        if (this.file != value) {
+            super.file = value;
+            $('#' + this.parent.id + '-filename').text(value);
+        }
+    }
+
+    private getModeByFileExtension(path) {
+        let list = ace.require("ace/ext/modelist");
+        return list.getModeForPath(path).mode;
+    }
+
+    public open() {
+        if (!this.file || this.file.length === 0 || !existsSync(this.file))
             return;
         this.$el.value = this.read();
         switch (path.extname(this.file)) {
@@ -142,24 +202,37 @@ export class CodeEditor extends EditorBase {
                 this.$session.setMode('ace/mode/lpc');
                 break;
             default:
-                this.$session.setMode('ace/mode/text');
+                this.$session.setMode(this.getModeByFileExtension(this.file));
                 break;
         }
         this.$session.setValue(this.$el.value);
         this.emit('opened');
+        this.state |= FileState.opened;
     }
 
-    get file():string {
-        return super.file;
+    public refresh() {
+        this.open();
+        this.emit('refreshed');
     }
 
-    set file(value: string) {
-        if (super.file != value) {
-            super.file = value;
-            this.openFile();
-        }
-    }    
+    public save() {
+        this.write(this.$session.getValue());
+        this.emit('saved');
+    }
+
+    public selected() {
+        return this.$session.getTextRange(this.$editor.getSelectionRange());
+    }
 }
 
 export class VirtualEditor extends EditorBase {
+    public createControl() { }
+
+    public refresh() { }
+
+    public open() { }
+
+    public save() { }
+
+    public selected() { }
 }
