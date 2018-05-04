8bb02bd9080fe7f8b1308171c175e0bf8fdef3d3
icewolfz <icewolfz@users.noreply.github.com>
2018-05-03 19:34:19 -0500

    Add insert formated text systems to convert pinkfish codes into html codes to work with tinymce

--- src/common/library.ts
+++ src/common/library.ts
@@ -880,3 +880,13 @@ export function capitalize(s) {
     }
     return s.join(" ");
 }
+
+export function invert(obj) {
+    var new_obj = {};
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            new_obj[obj[prop]] = prop;
+        }
+    }
+    return new_obj;
+};
