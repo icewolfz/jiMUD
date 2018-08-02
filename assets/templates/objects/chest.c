e20f5f6430ca68465beceae4321e637044c6837e
icewolfz <icewolfz@users.noreply.github.com>
2018-08-02 02:10:05 -0500

    Code editor: add chest template

---
+++ assets/templates/objects/chest.c
@@ -0,0 +1,23 @@
+/**
+ * Name
+ * 
+ * Description
+ * 
+ * @author {your name}
+ * @created {date}
+ * @typeof object
+ * @doc /doc/build/etc/object
+ */
+#include <std.h>
+
+inherit OBJ_CHEST;
+
+void create()
+{
+   ::create();
+   set_name("");
+   set_short("");
+   set_long("");
+   set_nouns("");
+   set_adjectives("");
+}
