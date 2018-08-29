be14d199ba1d8b1f967fe1432168aa4b1f3655ef
icewolfz <icewolfz@users.noreply.github.com>
2018-08-29 18:49:32 -0500

    Area designer: after edtiing notes have it update right away

--- src/common/editor/area.designer.ts
+++ src/common/editor/area.designer.ts
@@ -4049,6 +4049,7 @@ export class AreaDesigner extends EditorBase {
                                 if (notes.open)
                                     notes.close();
                                 notes.remove();
+                                ed.control.clearEditor();
                             });
                             notes.showModal();
                         }
