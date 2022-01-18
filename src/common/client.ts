299b73a24881c8276c3648a69ea143e54a45b33e
icewolfz <icewolfz@users.noreply.github.com>
2022-01-17 23:43:47 -0600

    Fixed issue with client.echo, client.sendCommand, and client.sendBackground when used in script type and passing non string values

--- src/common/client.ts
+++ src/common/client.ts
@@ -216,7 +216,7 @@ export class Client extends EventEmitter {
                     continue;
                 idx = this.profiles.items[keys[k]].triggers.indexOf(trigger);
                 //found trigger bail, or it will keep looking and k index will be wrong profile
-                if(idx !== -1)
+                if (idx !== -1)
                     break;
             }
         //check to be sure trigger found
@@ -977,6 +977,8 @@ export class Client extends EventEmitter {
         if (fore == null) fore = AnsiColorCode.LocalEcho;
         if (back == null) back = AnsiColorCode.LocalEchoBack;
         const codes = '\x1b[0,' + this.display.CurrentAnsiCode() + '\n';
+        //make its a string in case raw js passes number or something else
+        str = '' + str;
         if (str.endsWith('\n'))
             str = str.substr(0, str.length - 1);
         if (this.telnet.prompt && forceLine) {
@@ -1040,6 +1042,8 @@ export class Client extends EventEmitter {
             else
                 this._input.AddCommandToHistory(txt);
         }
+        //make its a string in case raw js passes number or something else
+        txt = '' + txt;
         if (!txt.endsWith('\n'))
             txt = txt + '\n';
         const data = { value: txt, handled: false };
@@ -1062,6 +1066,8 @@ export class Client extends EventEmitter {
             else
                 this._input.AddCommandToHistory(txt);
         }
+        //make its a string in case raw js passes number or something else
+        txt = '' + txt;
         if (!txt.endsWith('\n'))
             txt = txt + '\n';
         const data = { value: txt, handled: false };
