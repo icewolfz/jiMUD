c4dc2f09d5322568e9b29b367e3368b85ab38838
icewolfz <icewolfz@users.noreply.github.com>
2024-09-27 01:26:24 -0500

    Area Designer: Fix type check for monster editing

--- src/common/editor/area.designer.ts
+++ src/common/editor/area.designer.ts
@@ -5958,7 +5958,8 @@ export class AreaDesigner extends EditorBase {
                                     ed.focus();
                                 },
                                 valueChanged: e => {
-                                    e.type = getMonsterTypeFlags(this.getMonsterType(e.value.type));
+                                    if (e.id === 'mon-wiz-type')
+                                        e.type = getMonsterTypeFlags(this.getMonsterType(e.value));
                                 }
                             });
                         }
@@ -7401,7 +7402,8 @@ export class AreaDesigner extends EditorBase {
                                     ed.focus();
                                 },
                                 valueChanged: e => {
-                                    e.type = getMonsterTypeFlags(this.getMonsterType(e.value.type));
+                                    if (e.id === 'mon-wiz-type')
+                                        e.type = getMonsterTypeFlags(this.getMonsterType(e.value));
                                 }
                             });
                         }
